f32635c5cccf92518b90fc595c8ab8e9f833fca6
Grekoff18 <kolagrekalo@gmail.com>
2021-04-19 16:34:03 +0300

    feat: project finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
-      ),
-    );
+void main() => runApp(MagicBall());
 
 class MagicBall extends StatefulWidget {
   @override
@@ -12,9 +9,30 @@ class MagicBall extends StatefulWidget {
 }
 
 class _MagicBallState extends State<MagicBall> {
+  int ballNumber = 1;
+  void setRandomNumber () => setState(() => ballNumber = Random().nextInt(5) + 1);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Ask me anything",
+          ),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: Container(
+          color: Colors.blue,
+          child: Center(
+            child: TextButton(
+              onPressed: setRandomNumber,
+              child: Image.asset("images/ball$ballNumber.png"),
+            )
+          ),
+        ),
+      )
+    );
   }
 }
 
